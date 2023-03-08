922d3be522ceb756fdd208f41510b5d8ec18146d
David P. Chassin <dchassin@slac.stanford.edu>
2023-03-08 15:10:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 #
 # gridlabd utility installer
 #
+# Environment
+#
+#   GITHUB_REPO=slacgismo/gridlabd-utilities
+#	GITHUB_BRANCH=main
+#
 
-GITHUB_REPO=slacgismo/gridlabd-utilities
-
-if [ $# -eq 0 ]; then
-	echo "./install.sh NAME"
-fi
-
-curl -sL https://rawusercontent.github.com/$GITHUB_REPO/main/source/$1.py -o /usr/local/opt/gridlabd/current/share/$1.py
+curl -sL https://rawusercontent.github.com/${GITHUB_REPO:-slacgismo/gridlabd-utilities}/${GITHUB_BRANCH:-main}/source/utility.py -o /usr/local/opt/gridlabd/current/share/utility.py
